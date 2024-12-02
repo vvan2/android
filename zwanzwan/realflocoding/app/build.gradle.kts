91a7d2d953c005314178661d5b720955b3bf1ccc
Son Juwan <113279387+vvan2@users.noreply.github.com>
2024-12-02 15:58:52 +0900

    8주차 flo

---
+++ zwanzwan/realflocoding/app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.realflocoding"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.realflocoding"
+        minSdk = 33
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.circleindicator)
+    implementation(libs.androidx.room.common)
+    implementation(libs.androidx.room.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.room:room-runtime:2.6.1") // Room 런타임
+    implementation("androidx.room:room-ktx:2.6.1") // Room 코틀린 확장
+    kapt("androidx.room:room-compiler:2.6.1") // Room 컴파일러
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.0")
+}
